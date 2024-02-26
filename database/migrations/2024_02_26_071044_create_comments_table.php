73b794758e43a268cab1fe0d6274a9abd5eac822
nguyenanbinh <nguyenanbinh.98.dn@gmail.com>
2024-02-26 23:21:15 +0700

    Blog comment feature

---
+++ database/migrations/2024_02_26_071044_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('blog_id');
+            $table->string('user_avatar');
+            $table->string('user_name');
+            $table->unsignedTinyInteger('level')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
