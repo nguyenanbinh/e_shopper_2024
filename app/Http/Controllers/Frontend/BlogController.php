aaf38b048168cd2e2713c697e47dacbb14e00319
nguyenanbinh <nguyenanbinh.98.dn@gmail.com>
2024-02-29 15:09:57 +0700

    fix blog comment

--- app/Http/Controllers/Frontend/BlogController.php
+++ app/Http/Controllers/Frontend/BlogController.php
@@ -20,22 +20,20 @@ class BlogController extends Controller
 
     public function show(string $id)
     {
+        // dd(auth()->check());
         $blog = Blog::find($id);
         $pre =  Blog::where('id', '<', $blog->id)->max('id');
         $next =  Blog::where('id', '>', $blog->id)->min('id');
-        $avgRate = round(
-            Rate::where('blog_id', $blog->id)
-                ->orderBy('created_at', 'desc')
-                ->get()
-                ->avg('rate')
-        );
 
-        $totalRate = count(Rate::where('blog_id', $blog->id)
-        ->orderBy('created_at', 'desc')
-        ->get());
+        $rate = Rate::where('blog_id', $blog->id)->orderBy('created_at', 'desc')->get();
+        $avgRate = round($rate->avg('rate'));
+        $totalRate = count($rate);
+
+        $comments = Comment::where('blog_id', $blog->id)->where('level', 0)->orderBy('created_at', 'desc')->get();
+
         return view(
             'frontend.blog.detail',
-            compact('blog', 'pre', 'next', 'avgRate', 'totalRate')
+            compact('blog', 'pre', 'next', 'avgRate', 'totalRate', 'comments')
         );
     }
 
@@ -56,77 +54,49 @@ class BlogController extends Controller
         }
     }
 
-    public function ajaxComment(Request $request) {
+    public function ajaxComment(Request $request)
+    {
         $content = $request->content;
         $user = auth()->user();
         $request['user_avatar'] = $user->avatar;
         $request['user_name'] = $user->name;
-        $request['level'] = 0;
+        $request['level'] = $request->level;
+        $html = '';
+        $flag = false;
 
-        $comment = Comment::create($request->all());
-
-        if($comment) {
-            $hour = \Carbon\Carbon::parse($comment->created_at)->format('h:i a');
-            $date = \Carbon\Carbon::parse($comment->created_at)->format('M d, Y');
-            $html = <<<HTML
-                <li class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="$user->avatar_src" width="200" height="100" alt="">
-                    </a>
-                    <div class="media-body">
-                        <ul class="sinlge-post-meta">
-                            <li><i class="fa fa-user"></i>$user->name</li>
-                            <li><i class="fa fa-clock-o"></i>$hour</li>
-                             <li><i class="fa fa-calendar"></i>$date</li>
-                        </ul>
-                        <p>$content</p>
-                        <button class="btn btn-primary" class="reply-blog" data-comment-id="$comment->id"><i
-                                class="fa fa-reply"></i>Replay</button>
-                        <div class="replay-box1 hidden">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <h2>Leave a replay</h2>
-
-                                    <form id="postCommentChild">
-                                        <div class="text-area">
-                                            <div class="blank-arrow1">
-                                                <label>Your Name</label>
-                                            </div>
-                                             <span>*</span>
-                                            <textarea name="message" rows="5"></textarea>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">post comment</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            HTML;
-
-            return response()->json([
-                'html' => $html,
-                'message' => 'Comment success'
-            ], 201);
+        if ($request->level == 0) {
+            $flag = true;
+        } else {
+            $currentComment = Comment::find($request->level);
+            $flag = $currentComment && $currentComment->user_id != auth()->id();
         }
+
+        if ($flag == true) {
+            $comment = Comment::create($request->all());
+            if ($comment) {
+                $hour = \Carbon\Carbon::parse($comment->created_at)->format('h:i a');
+                $date = \Carbon\Carbon::parse($comment->created_at)->format('M d, Y');
+
+                $html = $this->generateCommentHtml($user, $comment, $content, $hour, $date);
+            }
+        }
+
+        return response()->json([
+            'html' => $html,
+            'message' => $flag ? 'Comment success' : 'Comment failed'
+        ], $flag ? 201 : 422);
     }
 
-    public function ajaxCommentChild(Request $request) {
-        $content = $request->content;
-        $user = auth()->user();
-        $request['user_avatar'] = $user->avatar;
-        $request['user_name'] = $user->name;
-        $request['level'] = $request->comment_id;
+    private function generateCommentHtml($user, $comment, $content, $hour, $date)
+    {
+        $avatarSrc = $user->avatar_src;
+        $htmlTemplate = '';
 
-        $comment = Comment::create($request->all());
-
-        if($comment) {
-            $hour = \Carbon\Carbon::parse($comment->created_at)->format('h:i a');
-            $date = \Carbon\Carbon::parse($comment->created_at)->format('M d, Y');
-            $html = <<<HTML
-                <li class="media second-media">
+        if ($comment->level == 0) {
+            $htmlTemplate = <<<HTML
+                <li class="media" data-comment-id="$comment->id">
                     <a class="pull-left" href="#">
-                        <img class="media-object" src="$user->avatar_src" width="200" height="100" alt="">
+                        <img class="media-object" src="$avatarSrc" width="200" height="100" alt="">
                     </a>
                     <div class="media-body">
                         <ul class="sinlge-post-meta">
@@ -136,13 +106,46 @@ class BlogController extends Controller
                         </ul>
                         <p>$content</p>
                     </div>
+                    <button class="btn btn-primary reply-blog"><i class="fa fa-reply"></i>Replay</button>
+                    <div class="replay-box1 hidden">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h2>Leave a replay</h2>
+                                <form id="postComment" data-level="$comment->id">
+                                    <div class="text-area">
+                                        <div class="blank-arrow1">
+                                            <label>Your Name</label>
+                                        </div>
+                                        <span>*</span>
+                                        <textarea name="message" rows="5"></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">post comment</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="children-comments">
+                    </ul>
                 </li>
             HTML;
-
-            return response()->json([
-                'html' => $html,
-                'message' => 'Comment success'
-            ], 201);
+        } else {
+            $htmlTemplate = <<<HTML
+                <li class="media second-media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="$comment->avatar_src" width="200" height="100" alt="">
+                    </a>
+                    <div class="media-body">
+                        <ul class="sinlge-post-meta">
+                            <li><i class="fa fa-user"></i>$comment->user_name</li>
+                            <li><i class="fa fa-clock-o"></i>$hour</li>
+                            <li><i class="fa fa-calendar"></i>$date</li>
+                        </ul>
+                        <p>$comment->content</p>
+                    </div>
+                </li>
+            HTML;
         }
+
+        return $htmlTemplate;
     }
 }
--- resources/views/frontend/blog/detail.blade.php
+++ resources/views/frontend/blog/detail.blade.php
@@ -14,17 +14,14 @@
                 <li><i class="fa fa-calendar"></i> {{ \Carbon\Carbon::parse($blog->created_at)->format('M d, Y') }}</li>
             </ul>
             <span>
-                @for ($i=1;$i<=5;$i++)
-                    @if($avgRate)
-                        @if($i <= $avgRate)
-                            <i class="fa fa-star"></i>
-                        @else
-                            <i class="fa fa-star-o"></i>
-                        @endif
+                @for ($i=1;$i<=5;$i++) @if($avgRate) @if($i <=$avgRate) <i class="fa fa-star"></i>
                     @else
-                        <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
                     @endif
-                @endfor
+                    @else
+                    <i class="fa fa-star-o"></i>
+                    @endif
+                    @endfor
             </span>
         </div>
         <a href="">
@@ -86,29 +83,31 @@
         @if(isset($comments) && count($comments))
             @foreach ($comments as $comment)
                 @php
-                    $childrenComments = $comment->children()->get();
-                    $hour = \Carbon\Carbon::parse($comment->created_at)->format('h:i a');
-                    $date = \Carbon\Carbon::parse($comment->created_at)->format('M d, Y');
+                $childrenComments = $comment->children()->get();
+                $hour = \Carbon\Carbon::parse($comment->created_at)->format('h:i a');
+                $date = \Carbon\Carbon::parse($comment->created_at)->format('M d, Y');
                 @endphp
-                <li class="media">
+                <li class="media" data-comment-id="{{ $comment->id }}">
                     <a class="pull-left" href="#">
                         <img class="media-object" src="{{ $comment->avatar_src }}" width="200" height="100" alt="">
                     </a>
                     <div class="media-body">
                         <ul class="sinlge-post-meta">
                             <li><i class="fa fa-user"></i>{{ $comment->user_name }}</li>
-                            <li><i class="fa fa-clock-o"></i> {{ \Carbon\Carbon::parse($comment->created_at)->format('h:i a') }}</li>
-                            <li><i class="fa fa-calendar"></i> {{ \Carbon\Carbon::parse($comment->created_at)->format('M d, Y') }}</li>
+                            <li><i class="fa fa-clock-o"></i> {{ \Carbon\Carbon::parse($comment->created_at)->format('h:i a') }}
+                            </li>
+                            <li><i class="fa fa-calendar"></i> {{ \Carbon\Carbon::parse($comment->created_at)->format('M d, Y')
+                                }}</li>
                         </ul>
                         <p>{{ $comment->content }}</p>
-                        <button class="btn btn-primary reply-blog" data-comment-id="{{ $comment->id }}"><i
+                        <button class="btn btn-primary reply-blog"><i
                                 class="fa fa-reply"></i>Replay</button>
                         <div class="replay-box1 hidden">
                             <div class="row">
                                 <div class="col-sm-12">
                                     <h2>Leave a replay</h2>
 
-                                    <form id="postCommentChild">
+                                    <form id="postComment" data-level="{{ $comment->id }}">
                                         <div class="text-area">
                                             <div class="blank-arrow1">
                                                 <label>Your Name</label>
@@ -122,22 +121,24 @@
                             </div>
                         </div>
                     </div>
+                    <ul class="children-comments">
+                        @foreach ($childrenComments as $child)
+                        <li class="media second-media">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" src="{{ $child->avatar_src }}" width="200" height="100" alt="">
+                            </a>
+                            <div class="media-body">
+                                <ul class="sinlge-post-meta">
+                                    <li><i class="fa fa-user"></i>{{ $child->user_name }}</li>
+                                    <li><i class="fa fa-clock-o"></i>{{ $hour }}</li>
+                                    <li><i class="fa fa-calendar"></i>{{ $date }}</li>
+                                </ul>
+                                <p>{{ $child->content }}</p>
+                            </div>
+                        </li>
+                        @endforeach
+                    </ul>
                 </li>
-                @foreach ($childrenComments as $child)
-                <li class="media second-media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="{{ $child->avatar_src }}" width="200" height="100" alt="">
-                    </a>
-                    <div class="media-body">
-                        <ul class="sinlge-post-meta">
-                            <li><i class="fa fa-user"></i>{{ $child->user_name }}</li>
-                            <li><i class="fa fa-clock-o"></i>{{ $hour }}</li>
-                            <li><i class="fa fa-calendar"></i>{{ $date }}</li>
-                        </ul>
-                        <p>{{ $child->content }}</p>
-                    </div>
-                </li>
-                @endforeach
             @endforeach
         @endif
     </ul>
@@ -147,7 +148,7 @@
     <div class="row">
         <div class="col-sm-12">
             <h2>Leave a replay</h2>
-            <form id="postCommentParent">
+            <form id="postComment" data-level="0">
                 <div class="text-area">
                     <div class="blank-arrow1">
                         <label>Your Name</label>
@@ -224,29 +225,51 @@
         });
 
         // Reply comment
-        $('form#postCommentParent').submit(function(e){
+        $('form#postComment').submit(function(e){
             e.preventDefault();
-            var content = $(this).find('textarea').val();
-            var commentList = $('.media-list');
-            $.ajax({
-                type:'POST',
-                url: '{{ route('blogs.ajaxComment') }}',
-                data: {
-                    _token: '{{ csrf_token() }}',
-                    content: content,
-                    blog_id: '{{ $blog->id }}',
-                    user_id: '{{ auth()->user()->id ?? '' }}',
-                },
-                success:function(data){
-                    commentList.append(data.html);
-                    console.log(data);
-                },
-                error: function(errors) {
-                    if(errors.status == 401) {
-                        alert('Please login to comment');
+            var isLogin = '{{ auth()->check() }}';
+            if(isLogin) {
+                var contentEle =  $(this).find('textarea');
+                var content = contentEle.val();
+                var commentList = $('.media-list');
+                var level = $(this).data('level');
+
+                $.ajax({
+                    type:'POST',
+                    url: '{{ route('blogs.ajaxComment') }}',
+                    data: {
+                        _token: '{{ csrf_token() }}',
+                        content: content,
+                        blog_id: '{{ $blog->id }}',
+                        user_id: '{{ auth()->user()->id ?? 0 }}',
+                        level
+                    },
+                    success:function(data){
+                        if(level == 0) {
+                            commentList.append(data.html);
+                        } else {
+                            var parentComment = $('li.media[data-comment-id="' + level + '"]');
+                            console.log(parentComment);
+                            parentComment.find('ul.children-comments').append(data.html);
+                            contentEle.val('');
+                        }
+
+                        alert(data.message);
+
+                    },
+                    error: function(errors) {
+                        var errMsg = errors.responseJSON.message;
+                        if(errMsg) {
+                            contentEle.val('');
+                            alert(errMsg);
+                        }
+                       console.log(errors);
                     }
-                }
-            });
+                });
+            } else {
+                alert('Please login to comment');
+            }
+
         });
 
         // Reply child
@@ -255,37 +278,37 @@
             $(this).parent().find('.replay-box1').toggleClass('hidden');
         });
 
-        $('form#postCommentChild').submit(function(e){
-            e.preventDefault();
-            var contentEle = $(this).find('textarea');
-            var content = contentEle.val();
-            // Get the closest parent 'li' element of the form
-            var parentLi = $(this).closest('li.media');
-            var mediaList = $(this).closest('ul.media-list');
-            var commentId = parentLi.find('.reply-blog').data('comment-id');
+        // $('form#postCommentChild').submit(function(e){
+        //     e.preventDefault();
+        //     var contentEle = $(this).find('textarea');
+        //     var content = contentEle.val();
+        //     // Get the closest parent 'li' element of the form
+        //     var parentLi = $(this).closest('li.media');
+        //     var mediaList = $(this).closest('ul.media-list');
+        //     var commentId = parentLi.find('.reply-blog').data('comment-id');
 
-            $.ajax({
-                type:'POST',
-                url: '{{ route('blogs.ajaxCommentChild') }}',
-                data: {
-                    _token: '{{ csrf_token() }}',
-                    content: content,
-                    comment_id: commentId,
-                    blog_id: '{{ $blog->id }}',
-                    user_id: '{{ auth()->user()->id ?? '' }}',
-                },
-                success:function(data){
-                    mediaList.append(data.html);
-                    contentEle.val('');
-                    console.log(data);
-                },
-                error: function(errors) {
-                    if(errors.status == 401) {
-                        alert('Please login to comment');
-                    }
-                }
-            });
-        });
+        //     $.ajax({
+        //         type:'POST',
+        //         url: '{{ route('blogs.ajaxCommentChild') }}',
+        //         data: {
+        //             _token: '{{ csrf_token() }}',
+        //             content: content,
+        //             comment_id: commentId,
+        //             blog_id: '{{ $blog->id }}',
+        //             user_id: '{{ auth()->user()->id ?? '' }}',
+        //         },
+        //         success:function(data){
+        //             mediaList.append(data.html);
+        //             contentEle.val('');
+        //             console.log(data);
+        //         },
+        //         error: function(errors) {
+        //             if(errors.status == 401) {
+        //                 alert('Please login to comment');
+        //             }
+        //         }
+        //     });
+        // });
 
     });
 </script>
